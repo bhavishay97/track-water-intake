245f61f640598cfddb1bc79f7d82a6df3ce3b490
Bhavishay Chawla <bhavishay97@outlook.com>
2020-06-28 12:54:15 +0530

    updated script.js
    Water logging is now working.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-alert('Working...');
+const smallCups = document.querySelectorAll('.small-cup');
+const litres = document.querySelector('.litres');
+const percentage = document.querySelector('.percentage');
+const remained = document.querySelector('.remained');
+const todaysGoalText = document.querySelector('h5');
+
+let totalWater = 3;
+let percentValue = 0;
+
+smallCups.forEach((cup, index) => {
+  cup.addEventListener('click', () => updateBigCup(index));
+});
+
+function updateBigCup(index) {
+  const logValue = parseInt(smallCups[index].innerHTML);
+
+  totalWater -= logValue / 1000;
+  litres.innerText = totalWater + ' L';
+
+  percentValue += (logValue / 3000) * 100;
+  percentage.innerText = percentValue.toFixed(1) + ' %';
+}
