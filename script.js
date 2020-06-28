6b3ccc22228d664d237b50821d3e24dd9fee3c1b
Bhavishay Chawla <bhavishay97@outlook.com>
2020-06-28 14:05:55 +0530

    added reset button

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const litres = document.querySelector('.litres');
 const percentage = document.querySelector('.percentage');
 const remained = document.querySelector('.remained');
 const todaysGoalText = document.querySelector('h5');
+const resetButton = document.querySelector('.reset-button');
 
 let totalWater = 3;
 let percentValue = 0;
@@ -10,6 +11,8 @@ let percentValue = 0;
 smallCups.forEach((cup, index) => {
   cup.addEventListener('click', () => updateBigCup(index));
 });
+
+resetButton.addEventListener('click', () => resetLogs());
 
 function updateBigCup(index) {
   const logValue = parseInt(smallCups[index].innerHTML);
@@ -19,4 +22,19 @@ function updateBigCup(index) {
 
   percentValue += (logValue / 3000) * 100;
   percentage.innerText = percentValue.toFixed(1) + ' %';
+
+  if (percentValue >= 99) {
+    litres.innerText = '0 L';
+    percentage.innerText = '100 %';
+    todaysGoalText.innerText =
+      'You have achieved your daily goal. Keep it up !';
+  }
+}
+
+function resetLogs() {
+  totalWater = 3;
+  percentValue = 0;
+  litres.innerText = '3.00 L';
+  percentage.innerText = '0 %';
+  todaysGoalText.innerText = `Today's goal is : 3 litres`;
 }
